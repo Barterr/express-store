b94c43fb5855775437ccd97378f9206995d0d929
Giuseppe <bartgb00@gmail.com>
2017-05-28 21:42:42 -0300

    Unique links for stores

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,5 +1,21 @@
 const mongoose = require('mongoose');
 const Store = mongoose.model('Store');
+const multer = require('multer');
+const jimp = require('jimp');
+const uuid = require('uuid');
+
+
+const multerOptions = {
+  storage: multer.memoryStorage(),
+  fileFilter(req, file, next) {
+    const isPhoto = file.mimetype.startsWith('image/');
+    if (isPhoto) {
+      next(null, true);
+    } else {
+      next({ message: 'That filetype isn\'t allowed!' }, false);
+    }
+  }
+};
 
 exports.homePage = (req, res) => {
   res.render('index');
@@ -11,6 +27,21 @@ exports.addStore = (req, res) => {
   });
 };
 
+exports.upload = multer(multerOptions).single('photo');
+
+exports.resize = async(req, res, next) => {
+  if (!req.file) {
+    next();
+    return;
+  }
+  const extension = req.file.mimetype.split('/')[1];
+  req.body.photo = `${uuid.v4()}.${extension}`;
+  const photo = await jimp.read(req.file.buffer);
+  await photo.resize(800, jimp.AUTO);
+  await photo.write(`./public/uploads/${req.body.photo}`);
+  next();
+};
+
 exports.createStore = async(req, res) => {
   const store = await (new Store(req.body)).save();
   req.flash('success', `Successfully Created ${store.name}. Care to leave a review?`);
@@ -18,6 +49,7 @@ exports.createStore = async(req, res) => {
 };
 
 exports.updateStore = async(req, res) => {
+  req.body.location.type = 'Point';
   const store = await Store.findOneAndUpdate({
     _id: req.params.storeId
   }, req.body, {
@@ -33,10 +65,14 @@ exports.getStores = async(req, res) => {
   res.render('stores', { title: 'Stores', stores });
 };
 
+exports.getStoreBySlug = async(req, res, next) => {
+  const store = await Store.findOne({ slug: req.params.storeSlug });
+  if (!store) return next();
+  res.render('store', { store, title: store.name });
+};
+
 exports.editStore = async(req, res) => {
   const store = await Store.findById(req.params.storeId);
   // TODO: Confirm they are owner of the store
   res.render('editStore', { title: `Edit ${store.name}`, store });
-
-  res.json(store);
 };
--- models/Store.js
+++ models/Store.js
@@ -13,16 +13,39 @@ const storeSchema = new mongoose.Schema({
     type: String,
     trim: true
   },
-  tags: [String]
+  tags: [String],
+  created: {
+    type: Date,
+    default: Date.now
+  },
+  location: {
+    type: {
+      type: String,
+      default: 'Point'
+    },
+    coordinates: [{
+      type: Number,
+      required: 'You must supply coordinates!'
+    }],
+    address: {
+      type: String,
+      required: 'You must supply an address!'
+    }
+  },
+  photo: String
 });
 
-storeSchema.pre('save', function(next) {
+storeSchema.pre('save', async function(next) {
   if (!this.isModified('name')) {
     return next();
   }
   this.slug = slug(this.name);
+  const slugRegEx = new RegExp(`^(${this.slug})((-\d+$)?)$`, 'i');
+  const storesWithSlug = await this.constructor.find({ slug: slugRegEx });
+  if (storesWithSlug.length) {
+    this.slug = `${this.slug}-${storesWithSlug.length+1}`;
+  }
   next();
-  // TODO: make sure slugs are unique
 });
 
 module.exports = mongoose.model('Store', storeSchema);
