dda9276e539d6b8dc7ab7bfb3861b30508ba64e3
Giuseppe <bartgb00@gmail.com>
2017-11-05 13:56:59 -0200

    Login and authentication

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const storeController = require('../controllers/storeController');
 const userController = require('../controllers/userController');
+const authController = require('../controllers/authController');
 const { catchErrors } = require('../handlers/errorHandlers');
 
 // Do work here
@@ -18,6 +19,7 @@ router.get('/store/:storeSlug',
 );
 
 router.get('/add',
+  authController.isLoggedIn,
   storeController.addStore
 );
 
@@ -41,10 +43,22 @@ router.get('/tags', catchErrors(storeController.getStoresByTag));
 router.get('/tags/:tag', catchErrors(storeController.getStoresByTag));
 
 router.get('/login', userController.loginForm);
+router.post('/login', authController.login);
 router.get('/register', userController.registerForm);
 
 
-router.post('/register', userController.validateRegister);
+router.post('/register',
+  userController.validateRegister,
+  catchErrors(userController.register),
+  authController.login
+);
 
+router.get('/logout', authController.logout);
+
+router.get('/account',
+  authController.isLoggedIn,
+  userController.account
+);
+router.post('/account', catchErrors(userController.updateAccount));
 
 module.exports = router;
